1b08b0090f74837c8e9244d043d1bb0112c4d831
morskoi den <deniscernysenko228@gmail.com>
2026-03-26 14:58:53 +0200

    Update ExpCommand.php

--- src/morskoi/ExpExchange/command/ExpCommand.php
+++ src/morskoi/ExpExchange/command/ExpCommand.php
@@ -5,10 +5,13 @@ namespace morskoi\ExpExchange\command;
 use pocketmine\command\{Command, CommandSender};
 use pocketmine\player\Player;
 use morskoi\ExpExchange\ExpExchange;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\plugin\PluginOwnedTrait;
 use pocketmine\scheduler\ClosureTask;
 
-class ExpCommand extends Command
+class ExpCommand extends Command implements PluginOwned
 {
+    use PluginOwnedTrait;
     private ExpExchange $plugin;
 
     public function __construct(ExpExchange $plugin)
@@ -16,12 +19,13 @@ class ExpCommand extends Command
         $this->plugin = $plugin;
         parent::__construct($this->plugin->getConfig()->get("command-name"), $this->plugin->getConfig()->get("command-description"), null, $this->plugin->getConfig()->get("command-aliases"));
         $this->setPermission("exp.cmd");
+        $this->owningPlugin = $plugin;
     }
     public function execute(CommandSender $s, string $label, array $args)
     {
         if (!$s instanceof Player)
         {
-            $s->sendMessage("Only in game");
+            $s->sendMessage("§cOnly in game");
             return;
         }
         $this->plugin->getScheduler()->scheduleDelayedTask(new ClosureTask(
@@ -30,6 +34,6 @@ class ExpCommand extends Command
                         $this->plugin->ExpMenu($s);
                     }
                 }
-            ), 20);
+        ), 20);
     }
 }
